b89a083fd0c031e0c0192918317dbb063e02b66e
daniel-deptula <daniel.deptula@gmail.com>
2024-09-16 21:36:29 +0200

    First draft of the script

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # tech-sbus-sniffer
 RS485 sniffer and parser of the Tech SBUS protocol
+
+# Overview
+TECH SBUS is a name used by vendor "Tech Sterowniki" for a custom communication protocol over RS-485, most probably developed by them. The protocol is used by their products - room sensors/regulators and central heating actuator controllers.
+
+This project is a very simple Python script that collects data sent over a RS-485 bus and extracts transmitted temperature and humidity measurements.
+
+It is a result of my reverse engineering of the protocol so only the limited functionality is available at the moment. If time permits new features may be added in future because much more data is transmitted. Everyone's encouraged to contribute.
+
+Devices used for testing:
+- L-X WiFi - central controller
+- R-12S - wired room regulator with air temperature sensor, humidity sensor and a connector for floor temperature NTC sensor
+
+# The protocol
+
+The data sent over a serial bus consists of ASCII characters.
+
+A single frame starts with a '>' (0x3E) and ends with a line feed (0x0A). Between these two characters there're two base64-encoded strings:
+1) everything without the last 6 characters: **the message**
+2) the last 6 characters (without "==" padding): **CRC-32 of the message**
+
+The structure of the message:
+
+| Length (bytes) | Function  |
+| ----------- | ----------- |
+| 4 | Source address (every regulator or controller has it's own 4-byte address)    |
+| 2 | ? (when measurements are sent it's 50 00; in some other messages it's E9 FD)  |
+| 4 | Destination address                                                           |
+| 2 | ? (looks like it's always the same as the two bytes after the source address) |
+| n | The data                                                                      |
+
+## The data
+### Temperature and humidity values
+One or multiple parameters can be included in one message. Three bytes mean the type of the parameter and are followed by two bytes with the value.
+Types of parameters:
+* 04 00 00 - room temperature
+* 04 01 00 - floor temperature?
+* 04 02 00 - humidity
+
+The values are two-byte integers which divided by 10 give temperature in Celsius degrees or humidity percentage.
+
+Example:
+```
+04 00 00 f2 00 04 01 00 04 01 04 02 00 4c 02
+^^^^^^^^ ^^^^^ ^^^^^^^^ ^^^^^ ^^^^^^^^ ^^^^^
+room     24,2  floor    26    humidity 58,8%
+temp     deg   temp     deg
+```
+### Other values
+There're many other types of information transmitted but I didn't have time to analyse them / figure out what is what. I noticed for example date and time transmitted regularly by the central controller as follows:
+```
+3f a1 2e d0 24 94 6e 38 00 00 00 00
+            ^^^^^^^^^^^
+            Seconds from the Unix Epoch
+            946770980 = Sat Jan  1 23:56:20 UTC 2000
+```
+
+# Script usage
+
+Once started, the script opens the serial port given as the first parameter and starts listening for the communication on the RS485 bus. The messages are saved to the log file specified as the second parameter. The default "debug" logging level logs all messages and a lot of details about the processing. The temperature and humidity values are logged with the "info" level in the following format (CSV):
+```
+type of data,unix timestamp (when data received),source device address,room temperature,floor temperature,humidity
+```
+Example:
+```
+MEASUREMENT,946700000.0300002,00-01-02-03,21.5,27.2,60.4
+```
+Currently the only way to reduce the logging verbosity is to change the following piece of code:
+```
+level=logging.DEBUG
+```
+to for example:
+```
+level=logging.INFO
+```
+## Starting the script
+```
+/usr/bin/python3 ./tech_sbus_sniffer.py /dev/ttyUSB0  ./environment.log
+                 ^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^  ^^^^^^^^^^^^^^^^
+                 script path            serial        log file path
+                                        device
+```
+## Serial devices
+I used USB RS485 serial interfaces with the following chips and they both work fine:
+- FT232RL
+- CH341
---
+++ tech_sbus_sniffer.py
@@ -0,0 +1,117 @@
+import serial
+import sys
+import time
+import base64
+import binascii
+import logging
+
+LOG_FORMAT = ('%(asctime)-15s %(levelname)-8s %(message)s')
+LOG_DATEFORMAT = ('%Y-%m-%d %H:%M:%S')
+
+class EnvData:
+    def __init__(self, msg):
+        self.msg = msg
+        self.read_data_from_msg()
+
+    def read_data_from_msg(self):
+        i = 0
+        while i+4 < len(self.msg):
+            if self.msg[i:i+3] == bytes([4, 0, 0]):
+                # Room temperature
+                self.room_temp = float(int.from_bytes(self.msg[i+3:i+5], byteorder='little', signed=False))/10
+                logger.debug("Room temperature: "+str(self.room_temp))
+            elif self.msg[i:i+3] == bytes([4, 1, 0]):
+                # Floor temperature
+                self.floor_temp = float(int.from_bytes(self.msg[i+3:i+5], byteorder='little', signed=False))/10
+                logger.debug("Floor temperature: "+str(self.floor_temp))
+            elif self.msg[i:i+3] == bytes([4, 2, 0]):
+                # Humidity
+                self.humidity = float(int.from_bytes(self.msg[i+3:i+5], byteorder='little', signed=False))/10
+                logger.debug("Humidity: "+str(self.humidity))
+            else:
+                # Unknown parameter
+                logger.debug('Unknown parameter: ' + self.msg[i:i+3].hex(' '))
+            i += 5
+
+class Message:
+    def __init__(self, msg):
+        self.timestamp = time.time()
+        self.msg = msg
+        self.parse_msg()
+        
+    def parse_msg(self):
+        if len(self.msg) > 12:
+            self.src_addr = self.msg[0:4]
+            self.dst_addr = self.msg[6:10]
+            logger.debug('Msg from: ' + self.src_addr.hex('-') + " to: " + self.dst_addr.hex('-'))
+
+            # I don't know what this value is
+            self.smth1 = self.msg[4:6]
+            self.smth2 = self.msg[10:12]
+            # But it's always set to 0x50 0x00 when environmental measurements data is sent
+            if self.smth1 == bytes([0x50, 0]):
+                logger.debug('The message contains env data: ' + self.msg[12:].hex(' '))
+                self.env_data = EnvData(self.msg[12:])
+        else:
+            logger.error("Message too short. Unable to parse.")
+
+if __name__ == "__main__":
+    try:
+        port = sys.argv[1]
+        logfile = sys.argv[2]
+    except IndexError:
+        print("Usage: python3 {} serial_device logfile".format(sys.argv[0]))
+        sys.exit(-1)
+        
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(filename=logfile, encoding='utf-8', format=LOG_FORMAT, datefmt=LOG_DATEFORMAT, level=logging.DEBUG)
+    
+    with serial.Serial(port, 115200, parity=serial.PARITY_EVEN, bytesize=serial.SEVENBITS, timeout=None) as serial_conn:
+        while True:
+			# Read until LF (0x0A)
+            msg = serial_conn.read_until(expected='\x0a'.encode('utf-8'), size=None)
+            if len(msg) > 1:
+                #Decoding the message
+                strmsg = msg[0:-1].decode('ascii')
+                logger.debug("Received: " + strmsg)
+                if len(strmsg) > 6:
+                    if strmsg[0] != '>':
+                        logger.error("ERROR: Missing message start!")
+                    #first char is ">"
+                    #"==" at the end of the base64-encoded string is missing
+                    #The last 6 characters are encoded CRC-32
+                    encmsg = strmsg[1:-6]
+                    enccrc = strmsg[-6:] + "=="
+                    logger.debug("Base64 encoded message: " + encmsg)
+                    logger.debug("Base64 encoded CRC-32: " + enccrc)
+                    try:
+                        decoded_msg = base64.b64decode(encmsg)
+                        logger.debug("Base64 decoded message: " + decoded_msg.hex(' '))
+                        decoded_crc = base64.b64decode(enccrc)
+                        #Compute CRC-32 of the decoded message
+                        crc = binascii.crc32(decoded_msg)
+                        if crc.to_bytes(4,byteorder='little',signed=False) == decoded_crc:
+                            logger.debug("CRC check pass")
+                            report = Message(decoded_msg)
+                            report_str = 'MEASUREMENT,'+str(report.timestamp)+","+report.src_addr.hex('-')+','
+                            if hasattr(report, 'env_data'):
+                                try:
+                                    report_str += str(report.env_data.room_temp)+','
+                                except AttributeError:
+                                    report_str += ','
+                                try:
+                                    report_str += str(report.env_data.floor_temp)+','
+                                except AttributeError:
+                                    report_str += ','
+                                try:
+                                    report_str += str(report.env_data.humidity)
+                                except AttributeError:
+                                    pass
+                            logger.info(report_str)
+                        else:
+                            logger.error("ERROR: CRC check failed")
+                    except:
+                        logger.error("ERROR: Unable to decode")
+                else:
+                    logger.error("ERROR: Message too short: " + str(len(strmsg)))
+    sys.exit(0)
